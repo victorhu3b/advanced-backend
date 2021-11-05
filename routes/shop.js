2bd29637e4c88f62c18f3955b7e54c58c9d6a36e
victorhu3b <victorhu3@berkeley.edu>
2021-11-04 23:42:29 -0700

    inital commit

---
+++ routes/shop.js
@@ -0,0 +1,177 @@
+// Filename : user.js
+
+const express = require("express");
+const { check, validationResult} = require("express-validator");
+const jwt = require("jsonwebtoken");
+const router = express.Router();
+
+const User = require("../model/User");
+const auth = require("../middleware/auth");
+
+router.post(
+    "/add", auth,
+    async (req, res) => {
+
+        const user = await User.findById(req.user.id);
+
+        user.shoppinglist.push(req.body.item);
+        user.save();
+        res.json(user.shoppinglist);
+    }
+);
+router.delete(
+    "/delete", auth,
+    async (req, res) => {
+        const user = await User.findById(req.user.id);
+
+        var idx = user.shoppinglist.indexOf(req.body.item);
+        if (idx > -1) {
+          user.shoppinglist.splice(idx, 1);
+        }
+        user.save()
+        res.json(user.shoppinglist);
+    }
+);
+router.get(
+    "/list", auth,
+    async (req, res) => {
+        const user = await User.findById(req.user.id);
+        res.json(user.shoppinglist);
+    }
+);
+
+router.post(
+    "/signup",
+    [
+        check("username", "Please Enter a Valid Username")
+        .not()
+        .isEmpty(),
+        //check("email", "Please enter a valid email").isEmail(),
+        check("password", "Please enter a valid password").isLength({
+            min: 6
+        })
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            });
+        }
+
+        const {
+            username,
+         //   email,
+            password
+        } = req.body;
+        try {
+            let user = await User.findOne({
+                //email
+                username
+            });
+            if (user) {
+                return res.status(400).json({
+                    msg: "User Already Exists"
+                });
+            }
+
+            user = new User({
+                username,
+                //email,
+                password
+            });
+
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+
+            await user.save();
+
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+
+            jwt.sign(
+                payload,
+                "randomString", {
+                    expiresIn: 10000
+                },
+                (err, token) => {
+                    if (err) throw err;
+                    res.status(200).json({
+                        token
+                    });
+                }
+            );
+        } catch (err) {
+            console.log(err.message);
+            res.status(500).send("Error in Saving");
+        }
+    }
+);
+
+router.post(
+    "/login",
+    [
+      //check("email", "Please enter a valid email").isEmail(),
+      check("username", "Please enter a valid email").not().isEmpty(),
+      check("password", "Please enter a valid password").isLength({
+        min: 6
+      })
+    ],
+    async (req, res) => {
+      const errors = validationResult(req);
+  
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array()
+        });
+      }
+  
+      //const { email, password } = req.body;
+      const { username, password } = req.body;
+      try {
+        let user = await User.findOne({
+          username
+        });
+        if (!user)
+          return res.status(400).json({
+            message: "User Not Exist"
+          });
+  
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch)
+          return res.status(400).json({
+            message: "Incorrect Password !"
+          });
+  
+        const payload = {
+          user: {
+            id: user.id
+          }
+        };
+  
+        jwt.sign(
+          payload,
+          "randomString",
+          {
+            expiresIn: 3600
+          },
+          (err, token) => {
+            if (err) throw err;
+            res.status(200).json({
+              token
+            });
+          }
+        );
+      } catch (e) {
+        console.error(e);
+        res.status(500).json({
+          message: "Server Error"
+        });
+      }
+    }
+  );
+
+module.exports = router;
